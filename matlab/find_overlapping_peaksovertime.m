3421e07e3583bf44b3e9c0002c42d25b129e36c0
jaakkopeltonen <jaakkopeltonen@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2011-01-05 18:15:14 +0000

    git-svn-id: https://svn.assembla.com/svn/mlprojects/pol2rnaseq@1168 c7ad7044-cb60-498d-a845-55a1d3ae732a

--- matlab/find_overlapping_peaksovertime.m
+++ matlab/find_overlapping_peaksovertime.m
@@ -1,24 +1,33 @@
 function overlappingpeaks=find_overlapping_peaksovertime(chr_index,allpeaks,peakextentmultiplier);
 
-overlappingpeaks=cell(1,3);
-
-foundpeakstarts;
-foundpeakends;
-foundpeakheights;
 
 n_timepoints=length(allpeaks);
 
-peaksused=cell(n_timepoints,1);
+index_peakstart=1;
+index_peakend=2;
+index_peakscore=3;
+index_summitstart=4;
+index_summitend=5;
+index_summitheight=6;
+index_npeaks=7;
+index_maxpeaks=8;
+index_linenumber=9;
+
+
+overlappingpeaks={};
+
 
 for t=1:n_timepoints,
   peaks=allpeaks{t};
   npeaks=length(peaks{chr_index,1});
 
-  % peaksused{t}=zeros(npeaks,1);
-
-  for i=1:npeaks,    
-    peakstart=peaks{chr_index,1}(i);
-    peakend=peaks{chr_index,1}(i);
+  for i=1:npeaks,
+    if mod(i,100)==0,
+      fprintf(1,'Processing overlaps starting from time point %d, peak %d of %d\n',t, i, npeaks);
+    end;
+ 
+    peakstart=peaks{chr_index,index_peakstart}(i);
+    peakend=peaks{chr_index,index_peakend}(i);
     peakcenter=(peakstart+peakend)/2;
 
     % extend peak according to multiplier from center
@@ -31,14 +40,14 @@ for t=1:n_timepoints,
 
     % find peaks in the other time points that overlap this one
     for t2=1:n_timepoints,
-      if t2~=t1,
+      if t2~=t,
 	peaks2=allpeaks{t2};
 	npeaks2=length(peaks2{chr_index,index_peakstart});
 
 	overlappingpeak{t2}=[];
 	for i2=1:npeaks2,    
 	  peakstart2=peaks2{chr_index,index_peakstart}(i2);
-	  peakend2=peaks{chr_index,index_peakend}(i2);
+	  peakend2=peaks2{chr_index,index_peakend}(i2);
 	  peakcenter2=(peakstart2+peakend2)/2;
 
 	  % extend peak according to multiplier from center
@@ -61,6 +70,8 @@ for t=1:n_timepoints,
 	end;
       end;
     end;
+
+    
     
     % Give different types of scores to the overlapping peak
     % -degree of overlap of peaks between time points
@@ -70,30 +81,47 @@ for t=1:n_timepoints,
     %  there are several)
     % -mean peak height over time
     % -variance of peak height over time
+
     
-    % degree of overlap (ignore time points without peaks)
+    % extent of all peaks (union) and degree of overlap (intersection). Ignore time points without peaks.
+    min_startpoint=inf;
     max_startpoint=-inf;
     min_endpoint=inf;
+    max_endpoint=-inf;
     for t2=1:n_timepoints,      
-      if length(overlappingpeaks{t2})>0,
+      if length(overlappingpeak{t2})>0,
         peaks2=allpeaks{t2};    
-	startpoint=min(peaks{chr_index,index_peakstart}(overlappingpeaks{t2});
-	endpoint=max(peaks{chr_index,index_peakend}(overlappingpeaks{t2});
-	if startpoint>max_startpoint, max_startpoint=startpoint, end;
-	if endpoint<min_endpoint, min_endpoint=endpoint, end;
+	startpoint=min(peaks2{chr_index,index_peakstart}(overlappingpeak{t2}));
+	endpoint=max(peaks2{chr_index,index_peakend}(overlappingpeak{t2}));
+	if startpoint>max_startpoint, max_startpoint=startpoint; end;
+	if startpoint<min_startpoint, min_startpoint=startpoint; end;
+	if endpoint<min_endpoint, min_endpoint=endpoint; end;
+	if endpoint>max_endpoint, max_endpoint=endpoint; end;
       end;      
     end;
-
+    degree_of_overlap = min_endpoint - max_startpoint;
+    peaks_extent = max_endpoint - min_startpoint;
+    
     % sum of scores (time points without peaks get zero score)
     scoresum=0;
     for t2=1:n_timepoints,      
-      if length(overlappingpeaks{t2})>0,
+      if length(overlappingpeak{t2})>0,
 	peaks2=allpeaks{t2};
-	score=max(peaks{chr_index,index_peakscore}(overlappingpeaks{t2});
+	score=max(peaks2{chr_index,index_peakscore}(overlappingpeak{t2}));
 	scoresum=scoresum+score;
       end;
     end;
-    
+
+
+    % compute the other scores too, TODO
+
+
+    % store the overlapping peak and its scores
+    peakandscores = {overlappingpeak, peaks_extent, degree_of_overlap, scoresum};
+    overlappingpeaks = {overlappingpeaks{:}, peakandscores};  
+    % it's inefficient to expand the array all the time like above,
+    % it would be better to expand it with ever larger increments like in read_peak_summit_files.m
+
   end;
   
 end;
