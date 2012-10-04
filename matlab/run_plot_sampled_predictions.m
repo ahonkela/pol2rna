0c1083bc722dae2ff7d892e6a7527b5e41b1f931
ahonkela <ahonkela@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2012-10-04 18:20:46 +0000

    HMC plotting code.
    git-svn-id: https://svn.assembla.com/svn/mlprojects/pol2rnaseq@2543 c7ad7044-cb60-498d-a845-55a1d3ae732a

---
+++ matlab/run_plot_sampled_predictions.m
@@ -0,0 +1,28 @@
+mybasedir_code='~/mlprojects/';
+
+% for kernel-level computations
+path1=[mybasedir_code 'kern/matlab/'];
+% for model-level computations
+path2=[mybasedir_code 'gpsim/matlab/'];
+% for optimiDefaultConstraint.m
+path3=[mybasedir_code 'optimi/matlab/'];
+% for lnDiffErfs.m
+path4=[mybasedir_code 'ndlutil/matlab/'];
+% for addPrior.m
+path5=[mybasedir_code 'prior/matlab/'];
+% for dist2.m
+path6=[mybasedir_code 'matlab/netlab/NETLAB3p3/'];
+% for modelTieParam.m
+path7=[mybasedir_code 'mltools/matlab/'];
+% for various experiment things
+path8=[mybasedir_code 'pol2rnaseq/matlab/'];
+
+addpath(path1,path2,path3,path4,path5,path6,path7,path8)
+
+g = importdata('~/mlprojects/pol2rnaseq/matlab/finished_genes_2012-09-25.txt');
+myI = mybase:mymod:length(g);
+
+for k=myI,
+  fprintf('Running gene %d/%d\n', find(k==myI), length(myI));
+  plot_sampled_predictions_file(g{k});
+end
