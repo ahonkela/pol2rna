21cb2520fb72f6d95ef90e929724f50569fb8947
jaakkopeltonen <jaakkopeltonen@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2013-02-06 12:32:54 +0000

    git-svn-id: https://svn.assembla.com/svn/mlprojects/pol2rnaseq@2719 c7ad7044-cb60-498d-a845-55a1d3ae732a

--- matlab/clustergenes.m
+++ matlab/clustergenes.m
@@ -1,4 +1,6 @@
-load all_profiles_2012-10-08b.mat
+addpath /share/mi/workspace/jtpelto/synergy/mlprojects/mlprojects/pol2rnaseq/matlab
+load /share/synergy/analyses/all_profiles_2012-11-21.mat
+cd /share/mi/workspace/jtpelto/synergy/synergy_data/analyses/clustering_2012_11_26
 
 % Normalize profiles to be zero mean and unit variance in both POL2 and mRNA 
 munormalized=mu;
@@ -20,9 +22,9 @@ mutest = munormalized(testsetI,featuresetI);
 % For each potential cluster number, train the GMM, then compute log-likelihood of held-out data
 niter = 100;
 
-maxclust=200;
+maxclust=50;
 loglikelihoods=zeros(maxclust,2);
-for nclust=68:maxclust,
+for nclust=1:maxclust,
   [clustmemberships,clustmeans,clustcovs,clustprior,totalloglikelihoodtrain] = gaussmixt(mutrain, ones(size(mutrain,1),1), nclust, niter, [],[],[]);
 
   [clustmemberships2,clustmeans2,clustcovs2,clustprior2,totalloglikelihoodtest] = gaussmixt(mutest, ones(size(mutest,1),1), nclust, 0, clustmeans,clustcovs,clustprior);
@@ -33,13 +35,31 @@ end;
 
 % Log-likelihood plot
 clf; plot(loglikelihoods(:,1),'r-'); hold on; plot(loglikelihoods(:,2),'g-');
+xlabel('Number of clusters'); ylabel('RED=train-likelihood,GREEN=test-likelihood');
 
 % BIC plot
 % clf; k0=[1:172]';k=k0.*(42 + 0*(42*41)/2+42 + (k0-1)./k0); plot(-2*loglikelihoods(:,1)+k*log(494),'r-'); hold on; plot(-2*loglikelihoods(:,2)+k*log(522),'g-');
 
-bestnclust=20;
-  [clustmemberships,clustmeans,clustcovs,clustprior,totalloglikelihoodtrain] = gaussmixt(munormalized(:,featuresetI), ones(size(munormalized,1),1), bestnclust, niter, [],[],[]);
 
+
+bestnclust=20;
+nrepeats=50,
+loglikelihoods2=zeros(nrepeats,1);
+clusterings=cell(nrepeats,4);
+for irepeat=1:nrepeats,
+  [clustmemberships,clustmeans,clustcovs,clustprior,totalloglikelihoodtrain] = gaussmixt(munormalized(:,featuresetI), ones(size(munormalized,1),1), bestnclust, niter, [],[],[]);
+  loglikelihoods2(irepeat)=totalloglikelihoodtrain;
+  clusterings{irepeat,1} = clustmemberships;
+  clusterings{irepeat,2} = clustmeans;
+  clusterings{irepeat,3} = clustcovs;
+  clusterings{irepeat,4} = clustprior;
+end;
+
+Ibest = find(loglikelihoods2==max(loglikelihoods2));
+clustmemberships=clusterings{Ibest,1};
+clustmeans=clusterings{Ibest,2};
+clustcovs=clusterings{Ibest,3};
+clustprior=clusterings{Ibest,4};
 
 
 hardmemberships = zeros(size(munormalized,1),1);
@@ -58,19 +78,25 @@ for k=1:bestnclust,
     hardclustvars(k,:) = var(munormalized(Ik,:));
 end;
 
+featuresetIPol2=[1:5:101];
+featuresetIRNA=[102:5:202];
+timeindices=[1:21]/22*1280;
 for k=1:bestnclust,
-    clf; errorbar([[1:5:101] [102:5:202]],hardclustmeans(k,featuresetI),sqrt(hardclustvars(k,featuresetI)));
+%    clf; errorbar([[1:5:101] [102:5:202]],hardclustmeans(k,featuresetI),sqrt(hardclustvars(k,featuresetI)));
+    clf; errorbar(timeindices,hardclustmeans(k,featuresetIPol2),sqrt(hardclustvars(k,featuresetIPol2)),'r-');
+    hold on; errorbar(timeindices+2,hardclustmeans(k,featuresetIRNA),sqrt(hardclustvars(k,featuresetIRNA)),'g-');
+
     title(sprintf('Cluster %d (%d members)\n',k,hardclustersizes(k)));
     print(sprintf('cluster%d_corrected.eps',k),'-depsc');
 end;
 
-save hardmemberships_corrected.txt hardmemberships -ASCII
+save hardmemberships_corrected_2012_11_26.txt hardmemberships -ASCII
 
 
 
 
-genenames=read_stringfile('~/tigretemp/hmcplots_2012_10_08/hmc_delay_prctiles_2012-10-08b.txt',[' ' 10 13]);
-f = fopen('clusterindices_corrected.txt','w');
+genenames=read_stringfile('~/tigretemp/hmcplots_2012_11_21/hmc_delay_prctiles_2012-11-21.txt',[' ' 10 13]);
+f = fopen('clusterindices_corrected_2012_11_26.txt','w');
 for k=1:size(hardmemberships,1),
   fprintf(f,'%s %d\n', genenames{k+1}{1}, hardmemberships(k));
 end;
