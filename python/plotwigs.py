793238cb5ad9ae5383a4a05cdb628a0988a67510
Antti Honkela <antti.honkela@hiit.fi>
2014-06-10 11:30:31 +0300

    Add actual plotting code.

--- python/plotwigs.py
+++ python/plotwigs.py
@@ -3,7 +3,9 @@ import re
 import numpy as np
 import math
 import cPickle
+from matplotlib import pyplot as plt
 
+SAVEPATH="/share/synergy/analyses/premrna/"
 DATAPATH="/cs/taatto/group/mlb/synergy/data/2012-03_RNA"
 PREMRNA_RE=re.compile(r""".*unspliced""")
 READLEN=35.0
@@ -46,10 +48,70 @@ def read_wig(fname, matchre):
                     curpos += 1
     return wigs
 
+def read_and_save():
+    f = os.listdir(DATAPATH)
+    wigs = [DATAPATH + '/' + x for x in f if x.endswith('.wig')]
 
-f = os.listdir(DATAPATH)
-wigs = [DATAPATH + '/' + x for x in f if x.endswith('.wig')]
+    w = [(x, read_wig(x, PREMRNA_RE)) for x in wigs]
+    with open(SAVEPATH + 'MCF7_wiggles.pyd', 'wb') as f:
+        cPickle.dump(w, f, 2)
+    return w
 
-w = [(x, read_wig(x, PREMRNA_RE)) for x in wigs]
-with open('MCF7_wiggles.pyd', 'wb') as f:
-    cPickle.dump(w, f, 2)
+def load_wigs():
+    with open(SAVEPATH + 'MCF7_wiggles.pyd', 'r') as f:
+        w = cPickle.load(f)
+    w.sort(key=lambda x: x[0])
+    return w
+
+def combine_wigs(w):
+    if len(w[0]) == 2:
+        w = [x[1] for x in w]
+    genes = set()
+    val = dict()
+    for k in w:
+        genes = genes.union(k.keys())
+    print len(genes), "genes in all"
+    for g in genes:
+        d = [x.get(g, np.zeros(0, dtype=float)) for x in w]
+        maxlen = max([len(x) for x in d])
+        v = np.zeros((maxlen, len(d)), dtype=float)
+        for i in range(len(d)):
+            v[0:len(d[i]), i] = d[i]
+        val[g] = v
+    return val
+
+def plot_one(u, gene=None, savefile=None):
+    fig, ax = plt.subplots(figsize = (6, 4))
+    u = u / np.max(u)
+    u = u + np.array(range(9, -1, -1))[np.newaxis, :]
+    ax.plot(u)
+    ax.set_yticks(np.arange(10)+0.5)
+    ax.set_yticklabels(('1280', '640', '320', '160', '80', '40', '20', '10', '5', '0'))
+    if gene is not None:
+        ax.set_title(gene)
+    fig.tight_layout()
+    if savefile is not None:
+        fig.savefig(savefile, format='png')
+    return fig, ax
+
+def plot_wigs(v, genes, savepath=None):
+    for g in genes:
+        plt.close()
+        try:
+            u = v[g + '_unspliced']
+            if savepath is not None:
+                plot_one(u, g, savepath+'/'+g+'_premrna.png')
+            else:
+                plot_one(u, g)
+        except:
+            print "Plotting gene", g, "failed (no data?)"
+
+
+w = load_wigs()
+v = combine_wigs(w)
+
+PLOTPATH="/share/synergy/analyses/premrna"
+with open(os.path.expanduser('~/Dropbox/projects/pol2rnaseq/interesting_genes.txt'), 'r') as f:
+    genes = [x.strip() for x in f.readlines()]
+
+plot_wigs(v, genes, PLOTPATH)
