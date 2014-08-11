f063ed0e766f5f593cb70d1aa9df1a0349c4cb4c
Antti Honkela <antti.honkela@hiit.fi>
2014-08-11 10:18:09 +0300

    Refactored and updated heatmap plotting code.

---
+++ matlab/do_plot_heatmaps.m
@@ -0,0 +1,76 @@
+function I_pcomb = do_plot_heatmaps(dataVals1, dataVals2, name, Iextra),
+
+PRINTSTYLE='-painters';
+
+sz = size(dataVals1);
+dataVals1 = dataVals1 ./ repmat(max(dataVals1, [], 2), [1, sz(2)]);
+dataVals2 = dataVals2 ./ repmat(max(dataVals2, [], 2), [1, sz(2)]);
+
+val1mean = mean(dataVals1 .* repmat(1:sz(2), [sz(1), 1]), 2);
+val2mean = mean(dataVals2 .* repmat(1:sz(2), [sz(1), 1]), 2);
+[~, I] = sort(val1mean + val2mean);
+plot_with_order(dataVals1, dataVals2, I);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_meansort.eps']);
+
+[~, I] = sort(val1mean);
+plot_with_order(dataVals1, dataVals2, I);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_pol2sort.eps']);
+
+[~, I] = sort(val2mean);
+plot_with_order(dataVals1, dataVals2, I);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_mrnasort.eps']);
+
+[~, peak1] = max(dataVals1, [], 2);
+[~, peak2] = max(dataVals2, [], 2);
+
+[~, I] = sort(peak1 + peak2);
+plot_with_order(dataVals1, dataVals2, I);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_pmeansort.eps']);
+
+[~, I] = sort(1000*peak1 + peak2);
+plot_with_order(dataVals1, dataVals2, I);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_pcombsort.eps']);
+
+I_pcomb = I;
+
+[~, I] = sort(peak1);
+plot_with_order(dataVals1, dataVals2, I);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_ppol2sort.eps']);
+
+[~, I] = sort(peak2);
+plot_with_order(dataVals1, dataVals2, I);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_pmrnasort.eps']);
+
+Y = pdist([dataVals1, dataVals2]);
+Z = linkage(Y, 'average');
+order = optimalleaforder(Z, Y);
+
+plot_with_order(dataVals1, dataVals2, order);
+print('-depsc2', PRINTSTYLE, [name, '_heatmap_hclustsort.eps']);
+
+if nargin > 3,
+  plot_with_order(dataVals1, dataVals2, Iextra);
+  print('-depsc2', PRINTSTYLE, [name, '_heatmap_extrasort.eps']);
+end
+
+function plot_with_order(dataVals1, dataVals2, I)
+
+assert(size(dataVals1, 2) == 101);
+t_tick = [0, 20, 80, 160, 320, 640, 1280];
+x_tick = sqrt(t_tick) * 100 / sqrt(1280) + 1;
+
+colormap('hot')
+subplot(1, 2, 1);
+imagesc(dataVals1(I, :))
+set(gca, 'YTick', [])
+set(gca, 'XTick', x_tick)
+set(gca, 'XTickLabel', t_tick)
+xlabel('t (min)')
+title('Pol-II')
+subplot(1, 2, 2);
+imagesc(dataVals2(I, :))
+set(gca, 'YTick', [])
+set(gca, 'XTick', x_tick)
+set(gca, 'XTickLabel', t_tick)
+xlabel('t (min)')
+title('mRNA')
---
+++ matlab/plot_heatmaps.m
@@ -0,0 +1,71 @@
+% Plot heatmaps
+
+% -----------------------------------------------------------------
+% GP POSTERIORS
+% -----------------------------------------------------------------
+
+id = '2013-08-30';
+
+profiles = load(['~/projects/pol2rnaseq/analyses/hmc_results/profiles/all_profiles_' id '.mat']);
+
+%t_pred = (((0:100)/100*sqrt(1280)).^2)';
+T_MIN = 300;
+t_pred = profiles.t_pred - T_MIN;
+realt = find(t_pred >= 0);
+
+mygenes = importdata('../R/analysed_genes.txt');
+
+[I, A, B] = intersect(mygenes, profiles.genes);
+
+dataVals1 = profiles.mu(B, realt);
+dataVals2 = profiles.mu(B, realt + length(profiles.t_pred));
+dataVals1(dataVals1(:) < 0) = 0;
+
+I_pcomb = do_plot_heatmaps(dataVals1, dataVals2, 'plots/profiles');
+
+% -----------------------------------------------------------------
+% DATA
+% -----------------------------------------------------------------
+
+datadir = '~/projects/pol2rnaseq/data/';
+
+%load h3k4me3_series.mat
+%load series_for_matti_ver3.mat
+%load('pol2_for_matti_ver3.mat', 'bininfo');
+%load('pol2_summaryseries_2012_09.mat');
+load([datadir, 'bininfo_dec2012_corrected.mat'], 'bininfo');
+load([datadir 'pol2_summaryseries_2013_01_02.mat']);
+%r = load('rna_new_data4.mat');
+%act = importdata('activeGenes_new.txt');
+normfacts = importdata([datadir 'rna_norm_factors.txt']);
+r = load([datadir 'info_gene_mean_var.mat']);
+
+mygenes = importdata('../R/analysed_genes.txt');
+
+[I, A, B] = intersect(ensg2int(r.geneID), bininfo(:, 5));
+
+J = zeros(length(mygenes), 1);
+for k=1:length(mygenes),
+  JJ = strcmp(mygenes{k}, r.geneID(A));
+  if any(JJ),
+    J(k) = find(JJ);
+  end
+end
+
+%interestinggenes = A; %find(r.pvals(A) < 0.1);
+%interestinggenes = find(r.pvals(A) < 0.9);
+%interestinggenes = find(sum(r.counts(A,:), 2) >= 1000);
+interestinggenes_rna = A(sort(J(find(J))));
+interestinggenes_pol2 = B(sort(J(find(J))));
+
+dataVals1=pol2_summaryseries(interestinggenes_pol2,:);
+dataVals2=r.mu(interestinggenes_rna,:) ./ repmat(normfacts', [length(interestinggenes_rna), 1]);
+
+t_data = [0, 5, 10, 20, 40, 80, 160, 320, 640, 1280];
+t_plot = linspace(0, sqrt(1280), 101).^2;
+[foo, J] = min(bsxfun(@(x, y) abs(x-y), t_data, t_plot'), [], 2);
+
+dataVals1 = dataVals1(:, J);
+dataVals2 = dataVals2(:, J);
+
+do_plot_heatmaps(dataVals1, dataVals2, 'plots/data', I_pcomb);
