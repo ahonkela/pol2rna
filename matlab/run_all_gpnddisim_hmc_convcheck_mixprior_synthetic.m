4aec02efab859d0d3f1083dcf773a589bdcc1512
Antti Honkela <antti.honkela@hiit.fi>
2014-10-21 16:16:25 +0300

    Use new data and GPmat code.

--- matlab/run_all_gpnddisim_hmc_convcheck_mixprior_synthetic.m
+++ matlab/run_all_gpnddisim_hmc_convcheck_mixprior_synthetic.m
@@ -24,20 +24,7 @@ if ~exist('use_uniform_priors', 'var'),
   use_uniform_priors = 1;
 end
 
-%mybasedir_code='/share/work/jtpelto/tempsynergy/';
-%mybasedir_code='/media/JPELTONEN4/mlprojects/';
-%mybasedir_code='~/synergy_data/tempcodebranch/';
-%mybasedir_code='/share/mi/workspace/jtpelto/synergy/mlprojects/mlprojects/';
-%mybasedir_code='~/jaakkos_files/synergy/mlprojects/';
-mybasedir_code='~/mlprojects/';
-
-%mybasedir_data='/share/work/jtpelto/tempsynergy/';
-%mybasedir_data='/share/work/jtpelto/synergy-data/';
-%mybasedir_data='/media/JPELTONEN4/synergy_data/';
-%mybasedir_data='~/jaakkos_files/synergy/synergy_data/';
-%mybasedir_data='~/synergy_data/';
 mybasedir_data='~/projects/pol2rnaseq/';
-
 mybasedir_analyses=mybasedir_data;
 %mybasedir_analyses='~/jaakkos_files/synergy/';
 
@@ -46,27 +33,19 @@ mybasedir_analyses=mybasedir_data;
 datadir=[mybasedir_data 'data/'];
 analysisdir=[mybasedir_analyses 'analyses/'];
 
-% for kernel-level computations
-path1=[mybasedir_code 'kern/matlab/']
-% for model-level computations
-path2=[mybasedir_code 'gpsim/matlab/'];
-% for optimiDefaultConstraint.m
-path3=[mybasedir_code 'optimi/matlab/'];
-% for lnDiffErfs.m
-path4=[mybasedir_code 'ndlutil/matlab/'];
-% for addPrior.m
-path5=[mybasedir_code 'prior/matlab/'];
-% for dist2.m
-path6=[mybasedir_code 'matlab/netlab/NETLAB3p3/'];
-% for modelTieParam.m
-path7=[mybasedir_code 'mltools/matlab/'];
-% for various experiment things
-path8=[mybasedir_code 'pol2rnaseq/matlab/'];
+% GPmat
+path1=['~/SheffieldML/GPmat/matlab/'];
+% logsumexp
+path2=['~/SheffieldML/GPmat/matlab/clutterDir/'];
+% pol2rna specific code
+path3=['~/github/pol2rna/matlab/'];
+% NETLAB for dist2.m
+path4=['~/mlprojects/matlab/netlab/NETLAB3p3/'];
 
-addpath(path1,path2,path3,path4,path5,path6,path7,path8)
+addpath(path1,path2,path3,path4)
 
 
-load('simulated_data_2013-08-09.mat');
+load('simulated_data_2014-10-21.mat');
 
 datasize = size(rnadata{dataset});
 
