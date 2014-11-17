f31a8e38ca59835eae5b33be8d70e9fb484a5e4e
Antti Honkela <antti.honkela@hiit.fi>
2014-11-17 14:44:34 +0200

    properly normalise the data

--- python/write_pol2_wigs.py
+++ python/write_pol2_wigs.py
@@ -5,6 +5,8 @@ import os
 TIMES = ['0000', '0005', '0010', '0020', '0040',
          '0080', '0160', '0320', '0640', '1280']
 
+NORM = 36
+
 def load_data(datapath=os.path.expanduser('~/projects/pol2rnaseq/')):
     bindata = scipy.io.loadmat(datapath + 'all_gene_pol2bins_2013_01_02.mat')
     bininfo = scipy.io.loadmat(datapath + 'bininfo_dec2012_corrected.mat')
@@ -13,15 +15,15 @@ def load_data(datapath=os.path.expanduser('~/projects/pol2rnaseq/')):
     d = dict()
     for k in range(len(genes)):
         if pol2bins[k,0].shape[1] > 0:
-            d[genes[k]] = np.hstack(pol2bins[k,:])
+            d[genes[k]] = np.hstack(pol2bins[k,:]/NORM)
     return d
 
 def write_bin_wigs(w):
     genes = sorted(w.keys())
     for i in range(len(TIMES)):
-        fname = 'MCF7_PolII_%s.wig' % TIMES[i]
+        fname = 'MCF7_PolII_t%smin.wig' % TIMES[i]
         with open(fname, 'w') as f:
             f.write('track type=wiggle_0 name="mywiggle" description="mywiggle" visibility=full\n')
             for gene in genes:
                 f.write('fixedStep chrom=%s start=1 step=200 span=200\n' % gene)
-                f.write('\n'.join([('%f' % x).replace('.000000', '') for x in w[gene][:,i]]) + '\n')
+                f.write('\n'.join([('%f' % x).rstrip('0').rstrip('.') for x in w[gene][:,i]]) + '\n')
