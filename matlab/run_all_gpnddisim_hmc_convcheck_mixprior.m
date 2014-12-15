13f94c48ff9f223829061ded6a13a63ffa37cc96
Antti Honkela <antti.honkela@hiit.fi>
2014-12-15 14:18:49 +0200

    use GPmat, adopt data indexing error fix

--- matlab/run_all_gpnddisim_hmc_convcheck_mixprior.m
+++ matlab/run_all_gpnddisim_hmc_convcheck_mixprior.m
@@ -24,18 +24,6 @@ if ~exist('do_not_normalise', 'var'),
   do_not_normalise = 0;
 end
 
-%mybasedir_code='/share/work/jtpelto/tempsynergy/';
-%mybasedir_code='/media/JPELTONEN4/mlprojects/';
-%mybasedir_code='~/synergy_data/tempcodebranch/';
-%mybasedir_code='/share/mi/workspace/jtpelto/synergy/mlprojects/mlprojects/';
-%mybasedir_code='~/jaakkos_files/synergy/mlprojects/';
-mybasedir_code='~/mlprojects/';
-
-%mybasedir_data='/share/work/jtpelto/tempsynergy/';
-%mybasedir_data='/share/work/jtpelto/synergy-data/';
-%mybasedir_data='/media/JPELTONEN4/synergy_data/';
-%mybasedir_data='~/jaakkos_files/synergy/synergy_data/';
-%mybasedir_data='~/synergy_data/';
 mybasedir_data='~/projects/pol2rnaseq/';
 
 mybasedir_analyses=mybasedir_data;
@@ -46,32 +34,25 @@ mybasedir_analyses=mybasedir_data;
 datadir=[mybasedir_data 'data/'];
 analysisdir=[mybasedir_analyses 'analyses/'];
 
-% for kernel-level computations
-path1=[mybasedir_code 'kern/matlab/']
-% for model-level computations
-path2=[mybasedir_code 'gpsim/matlab/'];
-% for optimiDefaultConstraint.m
-path3=[mybasedir_code 'optimi/matlab/'];
-% for lnDiffErfs.m
-path4=[mybasedir_code 'ndlutil/matlab/'];
-% for addPrior.m
-path5=[mybasedir_code 'prior/matlab/'];
-% for dist2.m
-path6=[mybasedir_code 'matlab/netlab/NETLAB3p3/'];
-% for modelTieParam.m
-path7=[mybasedir_code 'mltools/matlab/'];
-% for various experiment things
-path8=[mybasedir_code 'pol2rnaseq/matlab/'];
+% GPmat
+path1=['~/SheffieldML/GPmat/matlab/'];
+% logsumexp
+path2=['~/SheffieldML/GPmat/matlab/clutterDir/'];
+% pol2rna specific code
+path3=['~/github/pol2rna/matlab/'];
+% NETLAB for dist2.m
+path4=['~/mlprojects/matlab/netlab/NETLAB3p3/'];
 
-addpath(path1,path2,path3,path4,path5,path6,path7,path8)
+addpath(path1,path2,path3,path4)
 
 cd(datadir)
 %load h3k4me3_series.mat
 %load series_for_matti_ver3.mat
 %load('pol2_for_matti_ver3.mat', 'bininfo');
 %load('pol2_summaryseries_2012_09.mat');
-load('bininfo_dec2012_corrected.mat', 'bininfo');
-load('pol2_summaryseries_2013_01_02.mat');
+load('bininfo_nov2014_corrected.mat', 'bininfo_problemgenes');
+bininfo = bininfo_problemgenes;
+load('pol2_summaryseries_2014_11_19.mat');
 %r = load('rna_new_data4.mat');
 %act = importdata('activeGenes_new.txt');
 normfacts = importdata('rna_norm_factors.txt');
