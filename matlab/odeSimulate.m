4f15a948362dc0a5e28bf73950f93b35a3b446c4
Antti Honkela <antti.honkela@hiit.fi>
2015-05-20 09:59:06 +0300

    Simulate with a changing decay.

---
+++ matlab/odeSimulate.m
@@ -0,0 +1,54 @@
+function yout = odeSimulate(p, t_p, t_obs, params, doplot),
+
+if nargin < 5,
+  doplot = 0;
+end
+
+%rawparams = odeTransformParams(params);
+rawparams = params;
+
+[tout, yout] = ode45(@(t, y) delayode(t, y, rawparams, p, t_p), [t_obs], rawparams{5});
+
+if doplot,
+  [tout, yout] = ode45(@(t, y) delayode(t, y, rawparams, p, t_p), [min(t_obs), max(t_obs)], rawparams{5});
+  subplot(2, 1, 1);
+  plot(t_obs-min(t_obs), p, 'rx');
+  v = axis;
+  axis([0, 1300, 0, v(4)])
+  subplot(2, 1, 2);
+  plot(tout-min(t_obs), yout);
+  hold on
+  plot(t_obs-min(t_obs), m, 'gx');
+  hold off
+  v = axis;
+  axis([0, 1300, 0, v(4)])
+end
+
+
+function grad = delayode(t, m, params, p, t_p),
+
+beta0 = params{1};
+beta2 = params{2};
+beta = sqrt(beta2);
+%alpha = log(2)/params(3);
+if isscalar(params{3}),
+  alpha = params{3};
+else
+  if t < params{3}(1),
+    alpha = params{3}(2);
+  else
+    alpha = params{3}(3);
+  end
+end
+Delta = params{4};
+
+grad = beta0 + beta*joinTheDots(p, t_p, t-Delta) - alpha * m;
+
+
+function val = joinTheDots(p, t_p, t),
+
+if t < t_p(1),
+  val = p(1);
+else
+  val = interp1(t_p, p, t, 'linear');
+end
---
+++ matlab/simulate_data_changingD.m
@@ -0,0 +1,110 @@
+% Generate synthetic data based on TIPARP Pol2 profile
+% Antti Honkela, May 2013
+
+datapath = '~/projects/pol2rnaseq/data/';
+
+TIPARP='ENSG00000163659';
+load([datapath, 'pol2_summaryseries_2013_01_02.mat']);
+load([datapath, 'bininfo_dec2012_corrected.mat'], 'bininfo');
+
+TIPIND = find(bininfo(:, 5) == ensg2int(TIPARP));
+t = [0, 5, 10, 20, 40, 80, 160, 320, 640, 1280];
+t_test = 0:2.5:1280;
+t_gen = {[0, 5, 10, 20, 40, 80, 160, 320, 640, 1280], ...
+         [0, 5, 10, 20, 40, 60, 80, 120, 160, 240, 320, 400, 480, 560, 640], ...
+         [0, 5, 10, 20:20:160 200:40:400 480:80:640]};
+
+if ~exist('r', 'var'),
+  r = load([datapath, 'info_gene_mean_var.mat']);
+end
+
+myrna = r.mu(find(strcmp(r.geneID, TIPARP)), :);
+
+mypol = pol2_summaryseries(TIPIND, :);
+
+% Fit GP to pol2
+if ~exist('pol2fit', 'var'),
+  optionsGP = gpOptions('ftc');
+  
+  % The kernel used is a sum of an rbf, a bias and a white term. Bias and
+  % white help numerical stability during optimisation (this will also
+  % result in bigger error-bars in the end). Also try 'lin' or 'matern32'
+  % or even 'rbfperiodic' for the first component of the kernel.
+  optionsGP.kern = {'mlp','white'};
+  %optionsGP.scale2var1 = true;     % Scale outputs to variance 1.
+  modelGP = gpCreate(1, 1, sqrt(t'), mypol', optionsGP);
+  modelGP = gpOptimise(modelGP, 1, 1000);
+  % The model is now optimised. The following function just does standard
+  % GP prediction based on the optimised model and the test inputs.
+  [pol2fit, yVar] = gpPosteriorMeanVar(modelGP, sqrt(t_test'));
+  pol2fit = pol2fit - min(pol2fit);
+end
+% end GP fit
+
+Dvals_base = log(2) ./ [2 4 8 16 32 64]; %[0.32, 0.16, 0.08, 0.04, 0.02, 0.01];
+Dvals = cell(1, length(Dvals_base)*4);
+for l=1:length(Dvals_base),
+  Dvals{0*length(Dvals_base)+l} = [80, Dvals_base(l), Dvals_base(l)/2];
+  Dvals{1*length(Dvals_base)+l} = [80, Dvals_base(l), Dvals_base(l)/1.5];
+  Dvals{2*length(Dvals_base)+l} = [80, Dvals_base(l), Dvals_base(l)*2];
+  Dvals{3*length(Dvals_base)+l} = [80, Dvals_base(l), Dvals_base(l)*1.5];
+end
+Deltavals = [0 4 8 12];
+funcs = zeros(length(Dvals), length(Deltavals), length(t_test));
+
+for k=1:length(Dvals),
+  for l=1:length(Deltavals),
+    D = Dvals{k};
+    S = 0.03;
+    B = 0.005;
+    Delta = Deltavals(l);
+    m0 = 0.008 / D(2);
+    t0 = 0;
+
+    %pol2shift = [pol2fit(1)*ones(Delta, 1); pol2fit(1:end-Delta)];
+  
+    %m = m0 * exp(D*(t0-t_test)) + B/D * (1-exp(D*(t0-t_test))) + ...
+    %S*exp(-D*t_test) .* cumtrapz(exp(D*t_test) .* pol2shift');
+
+    % beta0, beta^2, alpha, Delta, m0
+    params = {B, 1/(2.5^2)*S^2, D, t_test(Delta+1), m0};
+    m2 = odeSimulate(pol2fit, t_test, t_test, params);
+    %funcs(k,l,:) = 2*m2 / sqrt(mean(m2.^2));
+    funcs(k,l,:) = 4*m2 / max(m2);
+  end
+end
+
+t_indices = zeros(size(t));
+for k=1:length(t),
+  t_indices(k) = find(t_test == t(k));
+end
+
+plot(sqrt(t_test(t_indices)), squeeze(funcs(:, 1, t_indices)));
+hold on;
+plot(sqrt(t_test), pol2fit / max(pol2fit), 'k--');
+plot(sqrt(t), myrna / max(myrna), 'k-.');
+hold off
+legend('t1/2=2', 't1/2=4', 't1/2=8', 't1/2=16', 't1/2=32', 't1/2=64', 'Pol2', 'RNA', 'Location', 'NorthEast')
+axis([0 sqrt(1280) 0 5])
+set(gca, 'XTick', sqrt(t));
+set(gca, 'XTickLabel', t);
+%set(gcf, 'PaperPosition', [0 0 14 10]); print -depsc2 tiparp_simulation.eps
+
+rnadata = cell(size(t_gen));
+pol2data = cell(size(t_gen));
+for k=1:length(t_gen),
+  t_indices = zeros(size(t_gen{k}));
+  for l=1:length(t_indices),
+    t_indices(l) = find(t_test == t_gen{k}(l));
+  end
+
+  pol2data{k} = pol2fit(t_indices);
+  rnadata{k} = funcs(:, :, t_indices);
+  randn('state', 42);
+  pol2noise = 0.1 * randn(size(pol2data{k}));
+  rnanoise = 0.1 * randn(1, 1, length(t_indices));
+  pol2data{k} = pol2data{k} + pol2noise;
+  rnadata{k} = rnadata{k} + repmat(rnanoise, [size(rnadata{k}, 1), size(rnadata{k}, 2), 1]);
+end
+
+save simulated_data_changingD.mat pol2data rnadata Dvals Deltavals t_gen
